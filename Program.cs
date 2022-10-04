f72e4f02bb6165d69dde7551a3dae5fe1c3a154a
Gabriel Torres <gabrieltlpires@gmail.com>
2022-10-03 22:30:15 -0300

    Adiciona a solução do exercício Número negativo

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+int numero;
+
+Console.WriteLine("------------------------------------------");
+Console.WriteLine("-----Descubra se o número é negativo-----");
+Console.WriteLine("------------------------------------------");
+
+Console.Write("Por favor, digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero < 0)
+
+{
+    Console.WriteLine($"O número {numero} é negativo." );
+}
+
+else
+
+{
+    Console.WriteLine($"O número {numero} não é negativo." );
+}
